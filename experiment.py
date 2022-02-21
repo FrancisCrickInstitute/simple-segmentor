6b4afd5f7f17db6c01ba3ac2802cdf6dca06f9fe
Jacob Howes <jacob.howes@crick.ac.uk>
2022-02-21 16:26:35 +0000

    Added type casting to values from yaml

--- experiment.py
+++ experiment.py
@@ -28,23 +28,24 @@ def run_experiment(config_filepath):
 	experiment_name, _ = os.path.splitext(os.path.basename(config_filepath))
 
 	if config["model"]["type"] == "UNet":
-		model = UNet(config["model"]["in_channels"], 1)
+		model = UNet(int(config["model"]["in_channels"]), 1)
 	else:
-		model = UNetInception(config["model"]["in_channels"], 1)
+		model = UNetInception(int(config["model"]["in_channels"]), 1)
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	optimizer = torch.optim.Adam(model.parameters(), lr=config["optimizer"]["lr"])
+	optimizer = torch.optim.Adam(model.parameters(), lr=float(config["optimizer"]["lr"]))
 
 	dataloader = get_dataloader(config["data"]["img_dir"],
 	                            config["data"]["label_dir"],
-	                            config["data"]["batch_size"],
+	                            int(config["data"]["batch_size"]),
 	                            shuffle=True)
 
 	trainer = Trainer(experiment_name, model, device,
-	                  optimizer, dataloader, config["num_epochs"],
+	                  optimizer, dataloader, int(config["num_epochs"]),
 	                  config["working_folder"])
 
 	trainer.train()
+
 
 if __name__ == '__main__':
 	run_experiment("initial.yml")
