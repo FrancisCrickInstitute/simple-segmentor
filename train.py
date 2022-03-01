42fbe827bfd5735e75f48bd4cfaceaad9d227294
Jacob Howes <jacob.howes@crick.ac.uk>
2022-03-01 15:06:50 +0000

    Added logging

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 import os
+import time
+
+from datetime import datetime
 
 # Add validation at the end of each epoch
 
@@ -31,6 +34,10 @@ class Trainer:
 		self.n_epochs = n_epochs
 		self.working_folder = working_folder
 
+		self.log_file = os.path.join(self.working_folder, self.name + '.log')
+		with open(self.log_file, 'w+') as f:
+			f.write('epoch,training loss, validation loss,train cpu time,train gpu time,val time, timestamp\n')
+
 	def normalize_func2d(self, x, y):
 		return ((x - 127)/128), (y > 0.5)
 
@@ -38,7 +45,7 @@ class Trainer:
 		model_save_folder = os.path.join(self.working_folder, 'model')
 
 		if val_loss < self.best_epoch['val_loss']:
-			self.best_epoch = {'epoch': self.epoch, 'train_loss': train_loss, val_loss: val_loss}
+			self.best_epoch = {'epoch': self.epoch, 'train_loss': train_loss, 'val_loss': val_loss}
 			files = os.listdir(model_save_folder)
 			for file in files:
 				filename, ext = os.path.splitext(file)
@@ -51,17 +58,27 @@ class Trainer:
 		model_checkpoint_filename = os.path.join(model_save_folder, f'{self.name}.{self.epoch}.pt')
 		torch.save(self.model.state_dict(), model_checkpoint_filename)
 
+	def append_to_log(self, train_loss, val_loss, cpu_time, gpu_time, val_time):
+		with open(self.log_file, 'a') as f:
+			f.write(f'{self.epoch},{train_loss:.4f},{val_loss:.4f},{cpu_time:.2f}s,{gpu_time:.2f}s,{val_time:.2f}s'
+			        f',{datetime.now()}\n')
+
+
 	def train(self):
 		print(f"=> Training...")
 		for self.epoch in range(self.epoch + 1, self.epoch + self.n_epochs + 1):
 			train_running_loss = 0
-			# _train_one_epoch, get loss, cpu_time, gpu_time
+			cpu_time = 0
+			gpu_time = 0
+
 			for i in range(self.train_sampler.steps_per_epoch):
-				#Add timing stuff
+				loop_iter_start_time = time.time()
 				x_batch, y_batch = self.train_sampler.sample()
 				
 				x_batch = (x_batch - 127) / 128
 				y_batch = (y_batch > 0.5)
+
+				cpu_cycle_done_time = time.time()
 
 				x_batch = torch.from_numpy(x_batch.astype(np.float32)).to(self.device)
 				y_batch = torch.from_numpy(y_batch.astype(np.float32)).to(self.device)
@@ -73,7 +90,17 @@ class Trainer:
 				self.optimizer.step()
 				train_running_loss += loss.item()
 
+				if self.device.type == 'cuda':
+					cpu_time += cpu_cycle_done_time - loop_iter_start_time
+					gpu_time += time.time() - cpu_cycle_done_time
+				else:
+					cpu_time += time.time() - loop_iter_start_time
+
+			self.total_cpu_time += cpu_time
+			self.total_gpu_time += gpu_time
+
 			val_running_loss = 0
+			val_start_time = time.time()
 			with torch.no_grad():
 				for i in range(self.val_sampler.steps_per_epoch):
 					x_batch, y_batch = self.val_sampler.sample()
@@ -87,7 +114,10 @@ class Trainer:
 					loss = dice_loss(y_pred, y_batch)
 					val_running_loss += loss.item()
 
+			val_time = time.time() - val_start_time
+
 			train_avg_loss = train_running_loss / self.train_sampler.steps_per_epoch
 			val_avg_loss = val_running_loss / self.val_sampler.steps_per_epoch
 			self.save_model_checkpoint(train_avg_loss, val_avg_loss)
+			self.append_to_log(train_avg_loss, val_avg_loss, cpu_time, gpu_time, val_time)
 			print(f"Epoch {self.epoch},\taverage train loss {train_avg_loss},\taverage val loss {val_avg_loss}")
