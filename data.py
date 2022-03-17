9a00ca3c7236285ab2cdd7732df21bc33411b5d7
Jacob Howes <jacob.howes@crick.ac.uk>
2022-03-17 16:41:59 +0000

    Data is shuffled if steps_per_val_epoch is set

--- data.py
+++ data.py
@@ -6,9 +6,9 @@ from torch.utils import data
 from skimage.io import imread
 
 
-def get_dataloader(image_path, label_path, patch_shape, batch_size):
+def get_dataloader(image_path, label_path, patch_shape, batch_size, shuffle):
     dataset = SequentialDataset(image_path, label_path, patch_shape)
-    return data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    return data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
 
 
 class SequentialDataset(data.Dataset):
