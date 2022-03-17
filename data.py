2e0667a770c7177ebb4e7f31e53598da71537e60
Luke Nightingale <luke.nightingale@crick.ac.uk>
2022-03-17 12:04:51 +0000

    tabs to spaces, remove unused file

--- data.py
+++ data.py
@@ -7,87 +7,87 @@ from skimage.io import imread
 
 
 def get_dataloader(image_path, label_path, patch_shape, batch_size, shuffle):
-	dataset = SequentialDataset(image_path, label_path, patch_shape)
-	return data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
+    dataset = SequentialDataset(image_path, label_path, patch_shape)
+    return data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
 
 
 class Sampler:
-	def __init__(self, image_path, label_path, patch_shape, batch_size, steps_per_epoch):
-		self.image_path = image_path
-		self.label_path = label_path
-		self.patch_shape = patch_shape
-		self.batch_size = batch_size
-		self.steps_per_epoch = steps_per_epoch
+    def __init__(self, image_path, label_path, patch_shape, batch_size, steps_per_epoch):
+        self.image_path = image_path
+        self.label_path = label_path
+        self.patch_shape = patch_shape
+        self.batch_size = batch_size
+        self.steps_per_epoch = steps_per_epoch
 
-		self.image_stack = imread(image_path)
-		self.label_stack = imread(label_path)
+        self.image_stack = imread(image_path)
+        self.label_stack = imread(label_path)
 
-	def random_patches(self):
-		rng = np.random.default_rng()
-		image_patches = np.zeros((1, *self.patch_shape))
-		label_patches = np.zeros((1, *self.patch_shape))
+    def random_patches(self):
+        rng = np.random.default_rng()
+        image_patches = np.zeros((1, *self.patch_shape))
+        label_patches = np.zeros((1, *self.patch_shape))
 
-		max_coords_for_patch_start = (
-			self.image_stack.shape[0] - self.patch_shape[0],
-			self.image_stack.shape[1] - self.patch_shape[1],
-			self.image_stack.shape[2] - self.patch_shape[2],
-		)
-		start_coords = rng.integers((0, 0, 0), max_coords_for_patch_start, endpoint=True)
-		image_patches[0, :, :, :] = self.image_stack[...,
-									start_coords[0]:start_coords[0] + self.patch_shape[0],
+        max_coords_for_patch_start = (
+            self.image_stack.shape[0] - self.patch_shape[0],
+            self.image_stack.shape[1] - self.patch_shape[1],
+            self.image_stack.shape[2] - self.patch_shape[2],
+        )
+        start_coords = rng.integers((0, 0, 0), max_coords_for_patch_start, endpoint=True)
+        image_patches[0, :, :, :] = self.image_stack[...,
+                                    start_coords[0]:start_coords[0] + self.patch_shape[0],
                                     start_coords[1]:start_coords[1] + self.patch_shape[1],
                                     start_coords[2]:start_coords[2] + self.patch_shape[2]
-		]
+        ]
 
-		label_patches[0, :, :, :] = self.label_stack[...,
-									start_coords[0]:start_coords[0] + self.patch_shape[0],
+        label_patches[0, :, :, :] = self.label_stack[...,
+                                    start_coords[0]:start_coords[0] + self.patch_shape[0],
                                     start_coords[1]:start_coords[1] + self.patch_shape[1],
                                     start_coords[2]:start_coords[2] + self.patch_shape[2]
-		]
-		return image_patches, label_patches
+        ]
+        return image_patches, label_patches
 
-	def sample(self):
-		image_patches = np.zeros((self.batch_size, *self.patch_shape))
-		label_patches = np.zeros((self.batch_size, *self.patch_shape))
+    def sample(self):
+        image_patches = np.zeros((self.batch_size, *self.patch_shape))
+        label_patches = np.zeros((self.batch_size, *self.patch_shape))
 
-		for i in range(self.batch_size):
-			rand_image, rand_label = self.random_patches()
-			image_patches[i, ...] = rand_image[0, ...]
-			label_patches[i, ...] = rand_label[0, ...]
+        for i in range(self.batch_size):
+            rand_image, rand_label = self.random_patches()
+            image_patches[i, ...] = rand_image[0, ...]
+            label_patches[i, ...] = rand_label[0, ...]
 
-		return image_patches, label_patches
+        return image_patches, label_patches
 
 
 class SequentialDataset(data.Dataset):
-	def __init__(self, image_path, label_path, patch_shape):
-		self.image_path = image_path
-		self.label_path = label_path
-		self.patch_shape = patch_shape
+    def __init__(self, image_path, label_path, patch_shape):
+        self.image_path = image_path
+        self.label_path = label_path
+        self.patch_shape = patch_shape
 
-		self.image_stack = imread(image_path)
-		self.label_stack = imread(label_path)
+        self.image_stack = imread(image_path)
+        self.label_stack = imread(label_path)
 
-		self.z_patches = self.image_stack.shape[0] // self.patch_shape[0]
-		self.x_patches = self.image_stack.shape[1] // self.patch_shape[1]
-		self.y_patches = self.image_stack.shape[2] // self.patch_shape[2]
+        self.z_patches = self.image_stack.shape[0] // self.patch_shape[0]
+        self.x_patches = self.image_stack.shape[1] // self.patch_shape[1]
+        self.y_patches = self.image_stack.shape[2] // self.patch_shape[2]
 
-	def __len__(self):
-		return self.z_patches * self.x_patches * self.y_patches
+    def __len__(self):
+        return self.z_patches * self.x_patches * self.y_patches
 
-	def __getitem__(self, idx):
-		y_patch = idx % self.y_patches
-		x_patch = (idx // self.y_patches) % self.x_patches
-		z_patch = (idx // (self.y_patches * self.x_patches)) % self.z_patches
+    def __getitem__(self, idx):
+        y_patch = idx % self.y_patches
+        x_patch = (idx // self.y_patches) % self.x_patches
+        z_patch = (idx // (self.y_patches * self.x_patches)) % self.z_patches
 
-		img_patch = self.image_stack[
-					z_patch * self.patch_shape[0]: (z_patch + 1) * self.patch_shape[0],
-					x_patch * self.patch_shape[1]: (x_patch + 1) * self.patch_shape[1],
-					y_patch * self.patch_shape[2]: (y_patch + 1) * self.patch_shape[2]
-		]
-		label_patch = self.label_stack[
-					z_patch * self.patch_shape[0]: (z_patch + 1) * self.patch_shape[0],
-					x_patch * self.patch_shape[1]: (x_patch + 1) * self.patch_shape[1],
-					y_patch * self.patch_shape[2]: (y_patch + 1) * self.patch_shape[2]
-		]
+        img_patch = self.image_stack[
+                    z_patch * self.patch_shape[0]: (z_patch + 1) * self.patch_shape[0],
+                    x_patch * self.patch_shape[1]: (x_patch + 1) * self.patch_shape[1],
+                    y_patch * self.patch_shape[2]: (y_patch + 1) * self.patch_shape[2]
+        ]
+        label_patch = self.label_stack[
+                    z_patch * self.patch_shape[0]: (z_patch + 1) * self.patch_shape[0],
+                    x_patch * self.patch_shape[1]: (x_patch + 1) * self.patch_shape[1],
+                    y_patch * self.patch_shape[2]: (y_patch + 1) * self.patch_shape[2]
+        ]
 
-		return img_patch, label_patch
+        return img_patch, label_patch
--- train.py
+++ train.py
@@ -9,229 +9,229 @@ from datetime import datetime
 # Add validation at the end of each epoch
 
 def recall(pred, y):
-	tp = torch.sum(torch.logical_and(pred, y))
-	fn = torch.sum(torch.logical_and(pred == 0, y == 1))
-	return float(tp/(tp + fn))
+    tp = torch.sum(torch.logical_and(pred, y))
+    fn = torch.sum(torch.logical_and(pred == 0, y == 1))
+    return float(tp/(tp + fn))
 
 def precision(pred, y):
-	tp = torch.sum(torch.logical_and(pred, y))
-	fp = torch.sum(torch.logical_and(pred == 1, y == 0))
-	return float(tp/(tp + fp))
+    tp = torch.sum(torch.logical_and(pred, y))
+    fp = torch.sum(torch.logical_and(pred == 1, y == 0))
+    return float(tp/(tp + fp))
 
 def iou(pred, y):
-	intersection = torch.sum(pred * y)
-	union = torch.sum(torch.logical_or(pred, y))
-	return intersection / union
+    intersection = torch.sum(pred * y)
+    union = torch.sum(torch.logical_or(pred, y))
+    return intersection / union
 
 def dice(pred, y, smooth=0):
-	intersection = torch.sum(pred * y)
-	return (2 * intersection + smooth) / (torch.sum(pred) + torch.sum(y) + smooth)
+    intersection = torch.sum(pred * y)
+    return (2 * intersection + smooth) / (torch.sum(pred) + torch.sum(y) + smooth)
 
 def dice_loss(pred, y, smooth=1):
-	return 1 - dice(pred, y, smooth=smooth)
+    return 1 - dice(pred, y, smooth=smooth)
 
 
 class Trainer:
-	def __init__(self, name, model, device, optimizer, train_dataloader,
-	             val_dataloader, n_epochs, working_folder, steps_per_train_epoch, steps_per_val_epoch):
-		self.bce = torch.nn.BCELoss()
+    def __init__(self, name, model, device, optimizer, train_dataloader,
+                 val_dataloader, n_epochs, working_folder, steps_per_train_epoch, steps_per_val_epoch):
+        self.bce = torch.nn.BCELoss()
 
-		self.epoch = 0
-		self.total_cpu_time = 0
-		self.total_gpu_time = 0
-		self.collected_loss = []
-		self.best_epoch = {'epoch': 0, 'train_loss': float('inf'), 'val_loss': float('inf')}
+        self.epoch = 0
+        self.total_cpu_time = 0
+        self.total_gpu_time = 0
+        self.collected_loss = []
+        self.best_epoch = {'epoch': 0, 'train_loss': float('inf'), 'val_loss': float('inf')}
 
-		self.name = name
-		self.model = model
-		self.device = device
-		self.optimizer = optimizer
-		self.train_dataloader = train_dataloader
-		self.val_dataloader = val_dataloader
-		self.n_epochs = n_epochs
-		self.working_folder = working_folder
-		self.steps_per_train_epoch = steps_per_train_epoch
-		self.steps_per_val_epoch = steps_per_val_epoch
+        self.name = name
+        self.model = model
+        self.device = device
+        self.optimizer = optimizer
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.n_epochs = n_epochs
+        self.working_folder = working_folder
+        self.steps_per_train_epoch = steps_per_train_epoch
+        self.steps_per_val_epoch = steps_per_val_epoch
 
-		self.log_file = os.path.join(self.working_folder, self.name + '.log')
-		with open(self.log_file, 'w+') as f:
-			f.write('epoch,training loss,validation loss,train cpu time,train gpu time,val time,timestamp\n')
+        self.log_file = os.path.join(self.working_folder, self.name + '.log')
+        with open(self.log_file, 'w+') as f:
+            f.write('epoch,training loss,validation loss,train cpu time,train gpu time,val time,timestamp\n')
 
-	def normalize_func2d(self, x, y):
-		return ((x - 127)/128), (y > 0.5)
+    def normalize_func2d(self, x, y):
+        return ((x - 127)/128), (y > 0.5)
 
-	def save_model_checkpoint(self, train_loss, val_loss):
-		model_save_folder = os.path.join(self.working_folder, 'model')
+    def save_model_checkpoint(self, train_loss, val_loss):
+        model_save_folder = os.path.join(self.working_folder, 'model')
 
-		if val_loss < self.best_epoch['val_loss']:
-			self.best_epoch = {'epoch': self.epoch, 'train_loss': train_loss, 'val_loss': val_loss}
-			files = os.listdir(model_save_folder)
-			for file in files:
-				filename, ext = os.path.splitext(file)
-				if filename.startswith(f'{self.name}.best.') and ext == 'pt':
-					os.remove(os.path.join(model_save_folder, file))
+        if val_loss < self.best_epoch['val_loss']:
+            self.best_epoch = {'epoch': self.epoch, 'train_loss': train_loss, 'val_loss': val_loss}
+            files = os.listdir(model_save_folder)
+            for file in files:
+                filename, ext = os.path.splitext(file)
+                if filename.startswith(f'{self.name}.best.') and ext == 'pt':
+                    os.remove(os.path.join(model_save_folder, file))
 
-			best_model_fname = os.path.join(model_save_folder, f'{self.name}.best.{self.epoch}.pt')
-			torch.save(self.model.state_dict(), best_model_fname)
+            best_model_fname = os.path.join(model_save_folder, f'{self.name}.best.{self.epoch}.pt')
+            torch.save(self.model.state_dict(), best_model_fname)
 
-		model_checkpoint_filename = os.path.join(model_save_folder, f'{self.name}.{self.epoch}.pt')
-		torch.save(self.model.state_dict(), model_checkpoint_filename)
+        model_checkpoint_filename = os.path.join(model_save_folder, f'{self.name}.{self.epoch}.pt')
+        torch.save(self.model.state_dict(), model_checkpoint_filename)
 
-	def append_to_log(self, train_loss, val_loss, cpu_time, gpu_time, val_time):
-		with open(self.log_file, 'a') as f:
-			f.write(f'{self.epoch},{train_loss:.4f},{val_loss:.4f},{cpu_time:.2f}s,{gpu_time:.2f}s,{val_time:.2f}s'
-			        f',{datetime.now()}\n')
+    def append_to_log(self, train_loss, val_loss, cpu_time, gpu_time, val_time):
+        with open(self.log_file, 'a') as f:
+            f.write(f'{self.epoch},{train_loss:.4f},{val_loss:.4f},{cpu_time:.2f}s,{gpu_time:.2f}s,{val_time:.2f}s'
+                    f',{datetime.now()}\n')
 
-	def evaluate(self, dataloader):
-		with torch.no_grad():
-			total_dice = 0
-			total_iou = 0
-			total_precision = 0
-			total_recall = 0
-			total_cpu_time = 0
-			total_gpu_time = 0
-			for x_batch, y_batch in dataloader:
-				cpu_start_time = time.time()
+    def evaluate(self, dataloader):
+        with torch.no_grad():
+            total_dice = 0
+            total_iou = 0
+            total_precision = 0
+            total_recall = 0
+            total_cpu_time = 0
+            total_gpu_time = 0
+            for x_batch, y_batch in dataloader:
+                cpu_start_time = time.time()
 
-				x_batch, y_batch = x_batch.type(torch.int), y_batch.type(torch.int)
-				x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
-				cpu_done_time = time.time()
+                x_batch, y_batch = x_batch.type(torch.int), y_batch.type(torch.int)
+                x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
+                cpu_done_time = time.time()
 
-				x_batch = x_batch.to(self.device)
-				y_batch = y_batch.to(self.device)
+                x_batch = x_batch.to(self.device)
+                y_batch = y_batch.to(self.device)
 
-				y_pred = self.model(x_batch)
-				total_dice += dice(y_pred, y_batch)
+                y_pred = self.model(x_batch)
+                total_dice += dice(y_pred, y_batch)
 
-				y_pred = y_pred > 0.5
-				y_batch = y_batch > 0.5
+                y_pred = y_pred > 0.5
+                y_batch = y_batch > 0.5
 
-				total_iou += iou(y_pred, y_batch)
-				total_precision += precision(y_pred, y_batch)
-				total_recall += recall(y_pred, y_batch)
+                total_iou += iou(y_pred, y_batch)
+                total_precision += precision(y_pred, y_batch)
+                total_recall += recall(y_pred, y_batch)
 
-				total_cpu_time += cpu_done_time - cpu_start_time
-				total_gpu_time += time.time() - cpu_done_time
+                total_cpu_time += cpu_done_time - cpu_start_time
+                total_gpu_time += time.time() - cpu_done_time
 
-			avg_dice = total_dice / len(dataloader)
-			avg_iou = total_iou / len(dataloader)
-			avg_precision = total_precision / len(dataloader)
-			avg_recall = total_recall / len(dataloader)
+            avg_dice = total_dice / len(dataloader)
+            avg_iou = total_iou / len(dataloader)
+            avg_precision = total_precision / len(dataloader)
+            avg_recall = total_recall / len(dataloader)
 
-			return {
-				"DICE": avg_dice,
-				"IoU": avg_iou,
-				"Precision": avg_precision,
-				"Recall": avg_recall,
-				"GPU time": total_gpu_time,
-				"CPU time": total_cpu_time
-			}
+            return {
+                "DICE": avg_dice,
+                "IoU": avg_iou,
+                "Precision": avg_precision,
+                "Recall": avg_recall,
+                "GPU time": total_gpu_time,
+                "CPU time": total_cpu_time
+            }
 
-	def save_best_epoch(self):
-		model_save_folder = os.path.join(self.working_folder, 'model')
-		files = os.listdir(model_save_folder)
-		best_epoch = 0
-		for file in files:
-			filename, ext = os.path.splitext(file)
-			if filename.startswith(f'{self.name}.best.') and ext == 'pt':
-				self.model.load_state_dict(torch.load(os.path.join(model_save_folder, file)))
-				best_epoch = int(filename.split(".")[-1])
-				break
-		
-		train_metrics = self.evaluate(self.train_dataloader)
-		val_metrics = self.evaluate(self.val_dataloader)
+    def save_best_epoch(self):
+        model_save_folder = os.path.join(self.working_folder, 'model')
+        files = os.listdir(model_save_folder)
+        best_epoch = 0
+        for file in files:
+            filename, ext = os.path.splitext(file)
+            if filename.startswith(f'{self.name}.best.') and ext == 'pt':
+                self.model.load_state_dict(torch.load(os.path.join(model_save_folder, file)))
+                best_epoch = int(filename.split(".")[-1])
+                break
 
-		results_file = os.path.join(self.working_folder, self.name + '.results')
-		with open(results_file, "w+") as f:
-			f.write(f"Best epoch: {best_epoch + 1}\n\n")
+        train_metrics = self.evaluate(self.train_dataloader)
+        val_metrics = self.evaluate(self.val_dataloader)
 
-			f.write("Best epoch training set results:\n")
-			f.write(f" - DICE score: {train_metrics['DICE']}\n")
-			f.write(f" - IoU score: {train_metrics['IoU']}\n")
-			f.write(f" - Precision: {train_metrics['Precision']}\n")
-			f.write(f" - Recall: {train_metrics['Recall']}\n")
-			f.write(f" - GPU time: {train_metrics['GPU time']}\n")
-			f.write(f" - CPU time: {train_metrics['CPU time']}\n\n")
+        results_file = os.path.join(self.working_folder, self.name + '.results')
+        with open(results_file, "w+") as f:
+            f.write(f"Best epoch: {best_epoch + 1}\n\n")
 
-			f.write("Best epoch validation set results:\n")
-			f.write(f" - DICE score: {val_metrics['DICE']}\n")
-			f.write(f" - IoU score: {val_metrics['IoU']}\n")
-			f.write(f" - Precision: {val_metrics['Precision']}\n")
-			f.write(f" - Recall: {val_metrics['Recall']}\n")
-			f.write(f" - GPU time: {val_metrics['GPU time']}\n")
-			f.write(f" - CPU time: {val_metrics['CPU time']}\n")
+            f.write("Best epoch training set results:\n")
+            f.write(f" - DICE score: {train_metrics['DICE']}\n")
+            f.write(f" - IoU score: {train_metrics['IoU']}\n")
+            f.write(f" - Precision: {train_metrics['Precision']}\n")
+            f.write(f" - Recall: {train_metrics['Recall']}\n")
+            f.write(f" - GPU time: {train_metrics['GPU time']}\n")
+            f.write(f" - CPU time: {train_metrics['CPU time']}\n\n")
 
-	def train(self):
-		print(f"Training dataset:\t{len(self.train_dataloader.dataset)} patches")
-		print(f"Validation dataset:\t{len(self.val_dataloader.dataset)} patches")
-		print(f"=> Training...")
-		for self.epoch in range(self.epoch + 1, self.epoch + self.n_epochs + 1):
-			train_running_loss = 0
-			cpu_time = 0
-			gpu_time = 0
+            f.write("Best epoch validation set results:\n")
+            f.write(f" - DICE score: {val_metrics['DICE']}\n")
+            f.write(f" - IoU score: {val_metrics['IoU']}\n")
+            f.write(f" - Precision: {val_metrics['Precision']}\n")
+            f.write(f" - Recall: {val_metrics['Recall']}\n")
+            f.write(f" - GPU time: {val_metrics['GPU time']}\n")
+            f.write(f" - CPU time: {val_metrics['CPU time']}\n")
 
-			for i, (x_batch, y_batch) in enumerate(self.train_dataloader):
-				if self.steps_per_train_epoch is not None and i >= self.steps_per_train_epoch:
-					break
-				loop_iter_start_time = time.time()
+    def train(self):
+        print(f"Training dataset:\t{len(self.train_dataloader.dataset)} patches")
+        print(f"Validation dataset:\t{len(self.val_dataloader.dataset)} patches")
+        print(f"=> Training...")
+        for self.epoch in range(self.epoch + 1, self.epoch + self.n_epochs + 1):
+            train_running_loss = 0
+            cpu_time = 0
+            gpu_time = 0
 
-				x_batch = x_batch.type(torch.int)
-				y_batch = y_batch.type(torch.int)
-				
+            for i, (x_batch, y_batch) in enumerate(self.train_dataloader):
+                if self.steps_per_train_epoch is not None and i >= self.steps_per_train_epoch:
+                    break
+                loop_iter_start_time = time.time()
 
-				x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
+                x_batch = x_batch.type(torch.int)
+                y_batch = y_batch.type(torch.int)
 
-				cpu_cycle_done_time = time.time()
 
-				x_batch = x_batch.to(self.device)
-				y_batch = y_batch.to(self.device)
+                x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
 
-				self.optimizer.zero_grad()
-				y_pred = self.model(x_batch)
-				loss = dice_loss(y_pred, y_batch)
-				loss.backward()
-				self.optimizer.step()
-				train_running_loss += loss.item()
+                cpu_cycle_done_time = time.time()
 
-				if self.device.type == 'cuda':
-					cpu_time += cpu_cycle_done_time - loop_iter_start_time
-					gpu_time += time.time() - cpu_cycle_done_time
-				else:
-					cpu_time += time.time() - loop_iter_start_time
+                x_batch = x_batch.to(self.device)
+                y_batch = y_batch.to(self.device)
 
-			self.total_cpu_time += cpu_time
-			self.total_gpu_time += gpu_time
+                self.optimizer.zero_grad()
+                y_pred = self.model(x_batch)
+                loss = dice_loss(y_pred, y_batch)
+                loss.backward()
+                self.optimizer.step()
+                train_running_loss += loss.item()
 
-			val_running_loss = 0
-			val_start_time = time.time()
-			with torch.no_grad():
-				for i, (x_batch, y_batch) in enumerate(self.val_dataloader):
-					if self.steps_per_val_epoch is not None and i >= self.steps_per_val_epoch:
-						break
-					x_batch = x_batch.type(torch.int)
-					y_batch = y_batch.type(torch.int)
-					
-					x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
+                if self.device.type == 'cuda':
+                    cpu_time += cpu_cycle_done_time - loop_iter_start_time
+                    gpu_time += time.time() - cpu_cycle_done_time
+                else:
+                    cpu_time += time.time() - loop_iter_start_time
 
-					x_batch = x_batch.to(self.device)
-					y_batch = y_batch.to(self.device)
+            self.total_cpu_time += cpu_time
+            self.total_gpu_time += gpu_time
 
-					y_pred = self.model(x_batch)
-					loss = dice_loss(y_pred, y_batch)
-					val_running_loss += loss.item()
+            val_running_loss = 0
+            val_start_time = time.time()
+            with torch.no_grad():
+                for i, (x_batch, y_batch) in enumerate(self.val_dataloader):
+                    if self.steps_per_val_epoch is not None and i >= self.steps_per_val_epoch:
+                        break
+                    x_batch = x_batch.type(torch.int)
+                    y_batch = y_batch.type(torch.int)
 
-			val_time = time.time() - val_start_time
-			
-			if self.steps_per_train_epoch is None:
-				train_avg_loss = train_running_loss / len(self.train_dataloader)
-			else:
-				train_avg_loss = train_running_loss / self.steps_per_train_epoch
-			
-			if self.steps_per_val_epoch is None:
-				val_avg_loss = val_running_loss / len(self.val_dataloader)
-			else:
-				val_avg_loss = val_running_loss / self.steps_per_val_epoch
-			self.save_model_checkpoint(train_avg_loss, val_avg_loss)
-			self.append_to_log(train_avg_loss, val_avg_loss, cpu_time, gpu_time, val_time)
+                    x_batch, y_batch = self.normalize_func2d(x_batch, y_batch)
 
-		self.save_best_epoch()
+                    x_batch = x_batch.to(self.device)
+                    y_batch = y_batch.to(self.device)
+
+                    y_pred = self.model(x_batch)
+                    loss = dice_loss(y_pred, y_batch)
+                    val_running_loss += loss.item()
+
+            val_time = time.time() - val_start_time
+
+            if self.steps_per_train_epoch is None:
+                train_avg_loss = train_running_loss / len(self.train_dataloader)
+            else:
+                train_avg_loss = train_running_loss / self.steps_per_train_epoch
+
+            if self.steps_per_val_epoch is None:
+                val_avg_loss = val_running_loss / len(self.val_dataloader)
+            else:
+                val_avg_loss = val_running_loss / self.steps_per_val_epoch
+            self.save_model_checkpoint(train_avg_loss, val_avg_loss)
+            self.append_to_log(train_avg_loss, val_avg_loss, cpu_time, gpu_time, val_time)
+
+        self.save_best_epoch()
