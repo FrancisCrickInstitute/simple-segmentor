c98f9473035f68248b65358cfc6c8f18bd6286d3
Luke Nightingale <luke.nightingale@crick.ac.uk>
2022-03-17 13:13:21 +0000

    clear out some more tabs

--- experiment.py
+++ experiment.py
@@ -10,91 +10,91 @@ from data import get_dataloader
 
 """
 Config:
-	- num_epochs
-	- patch shape (tuple)
-	- working_folder
-	- patch_shape (tuple)
-	- model
-		- type (UNet / UNetInception)
-		- start_iblock_channels
-		- num_down_blocks
-		- layers_per_block
-	- optimizer
-		- lr
-	- data
-		- train_image_path
-		- train_label_path
-		- val_image_path
-		- val_label_path
-		- batch_size
-		- steps_per_train_epoch
-		- steps_per_val_epoch
+    - num_epochs
+    - patch shape (tuple)
+    - working_folder
+    - patch_shape (tuple)
+    - model
+        - type (UNet / UNetInception)
+        - start_iblock_channels
+        - num_down_blocks
+        - layers_per_block
+    - optimizer
+        - lr
+    - data
+        - train_image_path
+        - train_label_path
+        - val_image_path
+        - val_label_path
+        - batch_size
+        - steps_per_train_epoch
+        - steps_per_val_epoch
 """
 
 
 def run_experiment(config_filepath):
-	if not os.path.exists(config_filepath):
-		print("Config filepath does not exist")
-		return
-	with open(config_filepath) as f:
-		config = yaml.load(f, Loader=yaml.FullLoader)
-	experiment_name, _ = os.path.splitext(os.path.basename(config_filepath))
-	working_folder = os.path.join("experiments", experiment_name)
+    if not os.path.exists(config_filepath):
+        print("Config filepath does not exist")
+        return
+    with open(config_filepath) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    experiment_name, _ = os.path.splitext(os.path.basename(config_filepath))
+    working_folder = os.path.join("experiments", experiment_name)
 
-	if not os.path.isdir(working_folder):
-		os.mkdir(working_folder)
-		os.mkdir(os.path.join(working_folder, 'model'))
-	elif not os.path.isdir(os.path.join(working_folder, 'model')):
-		os.mkdir(os.path.join(working_folder, 'model'))
+    if not os.path.isdir(working_folder):
+        os.mkdir(working_folder)
+        os.mkdir(os.path.join(working_folder, 'model'))
+    elif not os.path.isdir(os.path.join(working_folder, 'model')):
+        os.mkdir(os.path.join(working_folder, 'model'))
 
-	shutil.copy(config_filepath, os.path.join(working_folder, config_filepath))
+    shutil.copy(config_filepath, os.path.join(working_folder, config_filepath))
 
-	patch_shape = config["patch_shape"]
-	patch_shape = (int(patch_shape[0]), int(patch_shape[1]), int(patch_shape[2]))
+    patch_shape = config["patch_shape"]
+    patch_shape = (int(patch_shape[0]), int(patch_shape[1]), int(patch_shape[2]))
 
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	if config["model"]["type"] == "UNet":
-		model = UNet(patch_shape[0], patch_shape[0],
-		                      start_iblock_channels=config["model"]["start_iblock_channels"],
-		                      num_down_blocks=config["model"]["num_down_blocks"],
-		                      layers_per_block=config["model"]["layers_per_block"]).to(device)
-	else:
-		model = UNetInception(patch_shape[0], patch_shape[0],
-		                      start_iblock_channels=config["model"]["start_iblock_channels"],
-		                      num_down_blocks=config["model"]["num_down_blocks"],
-		                      layers_per_block=config["model"]["layers_per_block"]).to(device)
-	optimizer = torch.optim.Adam(model.parameters(), lr=float(config["optimizer"]["lr"]))
+    if config["model"]["type"] == "UNet":
+        model = UNet(patch_shape[0], patch_shape[0],
+                              start_iblock_channels=config["model"]["start_iblock_channels"],
+                              num_down_blocks=config["model"]["num_down_blocks"],
+                              layers_per_block=config["model"]["layers_per_block"]).to(device)
+    else:
+        model = UNetInception(patch_shape[0], patch_shape[0],
+                              start_iblock_channels=config["model"]["start_iblock_channels"],
+                              num_down_blocks=config["model"]["num_down_blocks"],
+                              layers_per_block=config["model"]["layers_per_block"]).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=float(config["optimizer"]["lr"]))
 
-	train_dataloader = get_dataloader(config["data"]["train_image_path"],
-	                                  config["data"]["train_label_path"],
-	                                  config["patch_shape"],
-	                                  config["data"]["batch_size"],
-	                                  shuffle=True)
+    train_dataloader = get_dataloader(config["data"]["train_image_path"],
+                                      config["data"]["train_label_path"],
+                                      config["patch_shape"],
+                                      config["data"]["batch_size"],
+                                      shuffle=True)
 
-	val_dataloader = get_dataloader(config["data"]["val_image_path"],
-	                                config["data"]["val_label_path"],
-	                                config["patch_shape"],
-	                                config["data"]["batch_size"],
-	                                shuffle=not "steps_per_val_epoch" in config["data"])
+    val_dataloader = get_dataloader(config["data"]["val_image_path"],
+                                    config["data"]["val_label_path"],
+                                    config["patch_shape"],
+                                    config["data"]["batch_size"],
+                                    shuffle=not "steps_per_val_epoch" in config["data"])
 
-	steps_per_train_epoch = int(config["data"]["steps_per_train_epoch"]) \
-		if "steps_per_train_epoch" in config["data"] else None
-	steps_per_val_epoch = int(config["data"]["steps_per_val_epoch"]) \
-		if "steps_per_val_epoch" in config["data"] else None
+    steps_per_train_epoch = int(config["data"]["steps_per_train_epoch"]) \
+        if "steps_per_train_epoch" in config["data"] else None
+    steps_per_val_epoch = int(config["data"]["steps_per_val_epoch"]) \
+        if "steps_per_val_epoch" in config["data"] else None
 
-	trainer = Trainer(experiment_name, model, device,
-	                  optimizer, train_dataloader, val_dataloader,
-	                  int(config["num_epochs"]),
-	                  working_folder,
-	                  steps_per_train_epoch,
-	                  steps_per_val_epoch)
+    trainer = Trainer(experiment_name, model, device,
+                      optimizer, train_dataloader, val_dataloader,
+                      int(config["num_epochs"]),
+                      working_folder,
+                      steps_per_train_epoch,
+                      steps_per_val_epoch)
 
-	trainer.train()
+    trainer.train()
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--config', type=str, help='config filepath')
-	args = parser.parse_args()
-	run_experiment(args.config)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, help='config filepath')
+    args = parser.parse_args()
+    run_experiment(args.config)
